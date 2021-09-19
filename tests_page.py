735f35065892e74a43c6acead82a3cc81575566a
Sônia <soniaelisabetth@gmail.com>
2021-09-19 15:52:16 -0300

    addin ct_002

--- tests_page.py
+++ tests_page.py
@@ -8,18 +8,28 @@ class TestsPage(Lib):
     __EMAIL_TEXTBOX =  By.ID, 'username'
     __SENHA_TEXTBOX = By.ID, 'password'
     __ACESSAR_BUTTON = By.CLASS_NAME, 'btn-primary'
+    __ESQUECEU_SENHA_BUTTON = By.XPATH, '//a[contains(text(), "Esqueceu o seu usuário ou senha?")]'
+    __ID_USUARIO_TEXTBOX = By.ID, 'id_username'
+    __BUSCAR_BUTTON_1 = By.NAME, 'submitbuttonusername'
 
     def __init__(self, driver):
         self.driver = driver
     
     def ct_001(self, user, password):
+        #Teste de login
         self.open_page(self.URL)
         self.fill(self.__EMAIL_TEXTBOX, user)
         self.fill(self.__SENHA_TEXTBOX, password)
         self.click(self.__ACESSAR_BUTTON)
         assert 'Meus cursos' in self.driver.title
     
-    def ct_002(self,):...
+    def ct_002(self, user):
+        #Alteração de usuário ou senha pela identificação do usuário
+        self.open_page(self.URL)
+        self.click(self.__ESQUECEU_SENHA_BUTTON)
+        self.fill(self.__ID_USUARIO_TEXTBOX, user)
+        self.click(self.__BUSCAR_BUTTON_1)
+        assert '' in self.driver.title
 
     def ct_003(self,):...
     
