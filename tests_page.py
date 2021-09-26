d075a4368892381250cdf4e97a148b6f7a5ae9d6
Sônia <soniaelisabetth@gmail.com>
2021-09-26 15:55:57 -0300

    .

--- tests_page.py
+++ tests_page.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from lib import Lib
 from time import sleep
+import pyautogui
 
 class TestsPage(Lib):
     'Coloquem os localizadores dos elementos aqui, vai facilitar!'
@@ -55,7 +56,6 @@ class TestsPage(Lib):
     __FILL_URL = By.XPATH, '//input[@id="fileurl"]'
     __ATV__TESTAGEM = By.XPATH, '//*[@id="module-122898"]/div/div/div[2]/div/a/span'
     __TEST_SEND_AGAIN = By.XPATH, '//*[@id="yui_3_17_2_1_1632675935201_861"]/a[1]/div[1]/div[3]'
-    __ICON_NOTIFICATION = By.XPATH, '//*[@id="nav-notification-popover-container"]/div[1]/i'
     __NOTIFICATION_CHOICE = By.XPATH, '//*[@id="popover-region-container-6150a8ed2ee9f6150a8ed05dd017"]/div[2]/div/div[1]/div[1]/a[1]/div[1]/div[2]'
     __SEARCH_COURSE = By.XPATH, '//*[@id="shortsearchbox"]'
     __GO_TO_COURSE = By.XPATH, '//*[@id="coursesearch"]/fieldset/button'
@@ -63,6 +63,26 @@ class TestsPage(Lib):
     __NIVELAMENTO = By.XPATH, '//*[@id="module-120647"]/div/div/div[2]/div/a/span'
     __NIVELAMENTO_CHECK = By.XPATH, '//*[@id="q10210:3_answer0"]'
     __UNIDADE_APRENDIZADO = By.XPATH, '//*[@id="section-4"]/div/div[1]/div[2]/button'
+    __CONF_ACESS = By.XPATH, '//*[@id="themesettings-control"]/span'
+    __XPATH_CX_DISLX = By.XPATH, '//*[@id="fonttype"]'
+    __XPATH_DISLX_SOP = By.XPATH, '//*[@id="fonttype"]/option[2]'
+    __CLASS_PERFIL = By.CLASS_NAME, 'userpicture defaultuserpic'
+    __XPATH_PREFERENCIAS = By.XPATH, '//*[@id="actionmenuaction-6"]'
+    __XPATH_IDIOMA_PREFERIDO = By.XPATH, '//*[@id="region-main"]/div/div/div/div/div[1]/div/div/div/div[3]/a'
+    __XPATH_CX_IDIOMA = By.XPATH, '//*[@id="id_lang"]'
+    __XPATH_INGLES = By.XPATH, '//*[@id="id_lang"]/option[1]'
+    __NAME_SAVE_IDIOMA = By.NAME, 'Salvar mudanças'
+    __XPATH_ICON_MSG = By.XPATH, '//*[@id="message-drawer-toggle-614f5910d1996614f5910a546218"]'
+    __XPATH_MSG_FAV = By.XPATH, '//*[@id="yui_3_17_2_1_1632590098721_72"]'
+    __XPATH_MSG_SELF_USER = By.XPATH, '//*[@id="view-overview-favourites-target-614f5910d6a43614f5910a546229"]/div[2]/a/div[1]/p'
+    __TESTE_SELF_TEXTBOX_ = By.XPATH, '//*[@id="message-drawer-614f5910d6a43614f5910a546229"]/div[3]/div[1]/div[1]/div[2]/textarea'
+    __ICON_BARRA_LATERAL = By.CLASS_NAME, 'btn nav-link float-sm-left mr-1'
+    __LINK_GEST_QUALD_SFT = By.LINK_TEXT, 'https://undbclassroom.undb.edu.br/course/view.php?id=3835'
+    __ICON_PARTICIPANTES = By.LINK_TEXT, 'https://undbclassroom.undb.edu.br/user/index.php?id=3835'
+    __LINK_MANUAL_DO_CASE = By.XPATH, '//*[@id="module-29817"]/div/div/div[2]/div/a/span'
+    __XPATH_DOWNLOAD_CASE_PAPPER = By.XPATH, '//*[@id="icon"]/iron-icon'
+    __LINK_MANUAL_PAPPER = By.XPATH, '//*[@id="module-29818"]/div/div/div[2]/div/a/span'
+    __ICON_ID_NOTAS = By.ID, 'actionmenuaction-4'
 
     def __init__(self, driver):
         self.driver = driver
@@ -317,16 +337,84 @@ class TestsPage(Lib):
         text = 'UNDB Classroom'
         assert text in self.driver.title
 
-    def ct_0031(self,):...
-    def ct_0032(self,):...
-    def ct_0033(self,):...
-    def ct_0034(self,):...
-    def ct_0035(self,):...
-    def ct_0036(self,):...
-    def ct_0037(self,):...
-    def ct_0038(self,):...
-    def ct_0039(self,):...
-    def ct_0040(self,):...
+    def ctt_0031(self):
+        #TESTAGEM DE FONTE DE DISLEXIA
+        self.ct_0001('user', 'password')
+        self.click(self.__CONF_ACESS)
+        self.click(self.__XPATH_CX_DISLX)
+        self.click(self.__XPATH_DISLX_SOP)
+        self.click(self.__XPATH_CX_DISLX)
+
+    def ctt_0032(self):
+        #SALVAMENTO DA FONTE DE DISLEXIA
+        self.ct_0001('user', 'password')
+        self.click(self.__CONF_ACESSIBILIDADE)
+        self.click(self.__XPATH_CX_DISLX)
+        sleep(3)
+        self.click(self.__XPATH_DISLX_SOP)
+        sleep(3)
+        self.click(self.__XPATH_CX_DISLX)
+        sleep(2)
+        pyautogui.moveTo(1280, 394)
+        pyautogui.doubleClick()
+    
+    def ctt_0033(self):
+        #TESTAGEM MUDAR IDIOMA DO SISTEMA
+        self.ct_0001('user', 'password')
+        self.click(self.__CLASS_PERFIL)
+        self.click(self.__XPATH_PREFERENCIAS)
+        self.click(self.__XPATH_IDIOMA_PREFERIDO)
+        self.click(self.__XPATH_CX_IDIOMA)
+        self.click(self.__XPATH_INGLES)
+        self.click(self.__XPATH_CX_IDIOMA)
+        self.click(self.__NAME_SAVE_IDIOMA)
+
+    def ctt_0034(self, test_text):
+        #TESTAR ENVIAR MENSAGEM PARA SI MESMO
+        self.ct_0001('user', 'password')
+        self.click(self.__XPATH_ICON_MSG)
+        self.click(self.__XPATH_MSG_FAV)
+        self.click(self.__XPATH_MSG_SELF_USER)
+        self.fill(self.__TESTE_SELF_TEXTBOX_, test_text)
+
+    def ctt_0035(self):
+        #TESTAGEM DE MENU LATERAL
+        self.ct_0001('user', 'password')
+        self.click(self.__ICON_BARRA_LATERAL)
+    
+    def ctt_0036(self):
+        #ACESSO AOS PARTICIPANTES DA DISCIPLINA
+        self.ct_0001('user', 'password')
+        self.click(self.__LINK_GEST_QUALD_SFT)
+        self.click(self.__ICON_BARRA_LATERAL)
+        self.click(self.__ICON_PARTICIPANTES)
+    
+    def ctt_0037(self):
+        #PREFERÊNCIAS DE USUÁRIO
+        self.ct_0001('user', 'password')
+        self.click(self.__CLASS_PERFIL)
+        self.click(self.__XPATH_PREFERENCIAS)
+    
+    def ctt_0038(self):
+        #DOWNLOAD MANUAL DO CASE
+        self.ct_0001('user', 'password')
+        self.click(self.__LINK_MANUAL_DO_CASE)
+        self.click(self.__XPATH_DOWNLOAD_CASE_PAPPER)
+        pyautogui.press('Enter')
+
+    def ctt_0039(self):
+        #DOWNLOAD DO MANUAL DO PAPPER
+        self.ct_0001('user', 'password')
+        self.click(self.__LINK_MANUAL_PAPPER)
+        self.click(self.__XPATH_DOWNLOAD_CASE_PAPPER)
+        pyautogui.press('Enter')
+
+    def ctt_0040(self):
+        #VISUALIZAÇÃO DE NOTAS
+        self.ct_0001('user', 'password')
+        self.click(self.__CLASS_PERFIL)
+        self.click(self.__ICON_ID_NOTAS)
+
     def ct_0041(self,):...
     def ct_0042(self,):...
     def ct_0043(self,):...
